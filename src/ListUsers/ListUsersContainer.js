76722895e42612e20ba6459e9c458cb23b7426c9
Sarvani Lakshmi Nimmagadda <sarvanin@hobasa.com>
2020-11-25 13:35:06 +0530

    local changes

--- src/ListUsers/ListUsersContainer.js
+++ src/ListUsers/ListUsersContainer.js
@@ -47,33 +47,25 @@ class ListUsersContainer extends React.Component {
   };
 
   addUser = (newUser) => {
-    console.log(this.state);
     console.log(newUser);
-    // Push newly created user to array of users in state.
-
-    //   axios
-    //     .post("http://localhost:3001/users", newUser)
-    //     .then((response) => {
-    //       // console.log(data);
-    //       // console.log(response.data);
-    //       // if (response.request.status) {
-    //       data.push(newUser);
-    //       this.setState({
-    //         data: data,
-    //         fname: "",
-    //         lname: "",
-    //         email: "",
-    //         all_countries: this.all_countries,
-    //         all_states: this.all_states[this.all_countries[0]],
-    //         country: this.all_countries[0],
-    //         state: this.all_states[this.all_countries[0]][0],
-    //         // visible: false,
-    //       });
-    //       // }
-    //     })
-    //     .catch((response) => {
-    //       console.log(response);
-    //     });
+    const { data } = this.state;
+    console.log(data);
+    axios
+      .post("http://localhost:3001/users", newUser)
+      .then((response) => {
+        console.log(data);
+        // console.log(response.data);
+        if (response.request.status) {
+          data.push(newUser);
+          console.log(data);
+          this.setState({
+            data,
+          });
+        }
+      })
+      .catch((response) => {
+        console.log(response);
+      });
     //   // } else {
     //   //   this.validator.showMessages();
     //   //   // rerender to show messages for the first time
